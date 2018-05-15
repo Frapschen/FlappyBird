829563880fc1abd80029d5421f60fd7e6467d755
Frapschen <35491170+Frapschen@users.noreply.github.com>
2018-05-15 09:29:44 +0800

    first cpp

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+﻿#include "FB_headfile.h"
+
+int main()
+{
+	bool isfirst=true;
+	initgraph(288, 512);   // 创建绘图窗口，大小为 288x512 像素
+	while (true)
+	{
+		play();
+	
+	}
+ 	_getch();
+	closegraph();
+
+
+	
+}
